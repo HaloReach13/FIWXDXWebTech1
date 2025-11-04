b1230e2534557d6b0393bad41f4fcf08c3393c4e
HaloReach13 <juhaszbazsi13@gmail.com>
2025-11-04 15:17:46 +0100

    Add footer
    Add a footer the FIWXDX_urlap html page.

--- FIWXDX_1029/FIWXDX_feldolgozo.php
+++ FIWXDX_1029/FIWXDX_feldolgozo.php
@@ -1,22 +1,33 @@
-<?php
-if(isset($_POST)){
-    echo "<h2>HTML űrlap</h2>"
+<html lang="en">
 
-    $nev = $_POST["nev"];
-    $pin = $_POST["pin"];
-    $fav_fruit = $_POST["fav_fruit"];
-    $age = $_POST["age"];
-    $feet_size = $_POST["feet_size"];
-    $confidence = $_POST["confidence"];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FIWXDX - Juhász Balázs Html űrlap</title>
+</head>
 
-    echo "<p><strong>Név:</strong> " . $nev . "</p>";
-    echo "<p><strong>PIN kód:</strong> " . $pin . "</p>";
-    echo "<p><strong>Kedvenc Gyümölcs:</strong> " . $fav_fruit . "</p>";
-    echo "<p><strong>Életkor:</strong> " . $age . "</p>";
-    echo "<p><strong>Lábméret:</strong> " . $feet_size . "</p>";
-    echo "<p><strong>Önbizalom:</strong> " . $confidence . "</p>";
-    } else   
-    {
+<body>
+    <?php
+    if (isset($_POST)) {
+        echo "<h2>HTML űrlap</h2>";
+
+        $nev = $_POST["nev"];
+        $pin = $_POST["pin"];
+        $fav_fruit = $_POST["fav_fruit"];
+        $age = $_POST["age"];
+        $feet_size = $_POST["feet_size"];
+        $confidence = $_POST["confidence"];
+
+        echo "<p><strong>Név:</strong> " . $nev . "</p>";
+        echo "<p><strong>PIN kód:</strong> " . $pin . "</p>";
+        echo "<p><strong>Kedvenc Gyümölcs:</strong> " . $fav_fruit . "</p>";
+        echo "<p><strong>Életkor:</strong> " . $age . "</p>";
+        echo "<p><strong>Lábméret:</strong> " . $feet_size . "</p>";
+        echo "<p><strong>Önbizalom:</strong> " . $confidence . "</p>";
+    } else {
         echo "<h2><strong>Űrlap nem lett beküldve</strong></h2>";
-}
-?>
+    }
+    ?>
+</body>
+
+</html>
